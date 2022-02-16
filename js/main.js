4668157f42b8ef27f4134494c55fa54be6964f58
DeeM <49118076+oxyhertz@users.noreply.github.com>
2022-02-16 12:04:20 +0200

    render meme

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ var gElCanvas;
 
 function init() {
   createCanvas();
-  renderMeme('images/meme-imgs/1.jpg');
+  resizeCanvas();
+  renderMeme();
 }
 
 function createCanvas() {
@@ -13,25 +14,47 @@ function createCanvas() {
   gCtx = gElCanvas.getContext('2d');
 }
 
-function renderMeme(imgUrl) {
-  var img = new Image();
-  img.src = imgUrl;
-  img.onload = () => {
-    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
-  };
-  drawText(line);
+function resizeCanvas() {
+  var elContainer = document.querySelector('.canvas-container');
+  gElCanvas.width = elContainer.offsetWidth;
+  gElCanvas.height = elContainer.offsetHeight;
 }
 
-function drawLine() {}
+function renderMeme() {
+  var meme = getMeme();
+  drawImg2(meme);
+  drawLines(meme);
+}
+
+function onAddText() {
+  const txt = document.querySelector('.input-txt').value;
+}
+
+function drawImg2(meme) {
+  var img = new Image();
+  img.src = `./images/meme-imgs/${meme.selectedImgId}.jpg`;
+
+  gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
+}
+
+function drawLines(meme) {
+  meme.lines.forEach(line => {
+    drawText(line);
+  });
+}
 
 function drawText(line) {
   var txt = line.txt;
   gCtx.lineWidth = 2;
   gCtx.font = `${line.size}px ${line.font}`;
   gCtx.fillStyle = line.color;
-  gCtx.strokeStyle = 'black';
   gCtx.textAlign = line.align;
+  gCtx.strokeStyle = 'black';
   line.width = gCtx.measureText(txt).width;
-  gCtx.fillText(txt, gElCanvas.width / 2, gElCanvas.height - 30);
-  gCtx.strokeText(txt, gElCanvas.width / 2, gElCanvas.height - 30);
+  gCtx.fillText(txt, 50, 30);
+  gCtx.strokeText(txt, 50, 30);
 }
+// function drawImg() {
+//   var elImg = document.querySelector('img');
+//   gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height);
+// }
--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -1,17 +1,21 @@
 'use strict';
 var gImgs = [
-  { id: 1, url: 'images/meme-imgs/1.jpg', keywords: ['funny', 'cat'] },
+  { id: 1, url: './images/meme-imgs/1.jpg', keywords: ['funny', 'cat'] },
 ];
 var gMeme = {
-  selectedImgId: 5,
+  selectedImgId: 1,
   selectedLineIdx: 0,
   lines: [
     {
       txt: 'I sometimes eat Falafel',
-      size: 20,
+      size: 30,
       align: 'left',
       color: 'red',
       font: 'Arial',
     },
   ],
 };
+
+function getMeme() {
+  return gMeme;
+}
